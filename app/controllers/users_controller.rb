6a2195fb95437e3e01fd84aa0a65cd15e7b75d30
takumagoo <117556435+takumagoo@users.noreply.github.com>
2022-12-27 13:25:30 +0000

    [add] 4-27 フラッシュメッセージ途中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
     @users = User.new(post_image_params)
     @users.user_id = current_user.id
     @users.save
+    flash[:notice] = "You have created book successfully."
     # redirect_to post_images_path
   end
 
@@ -16,12 +17,17 @@ class UsersController < ApplicationController
 
   def show
     @book = Book.new
+    @books = Book.new
+    @books = Book.all
+
+    @user = User.new
+    @user = User.find(params[:id])
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def get_image
     unless image.attached?
       file_path = Rails.root.join('app/assets/images/no_image.jpg')
@@ -29,10 +35,11 @@ class UsersController < ApplicationController
     end
     image
   end
-  
+
   def update
     user = User.find(params[:id])
     user.update(user_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to user_path(user.id)
   end
 
